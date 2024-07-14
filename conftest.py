356c05a9a5edf3d5bb99181af106a10dade7e639
Oleg Peshko <opeshko@ptsecurity.com>
2024-07-14 14:12:32 +0700

    Uncomment headless option for driver

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture()
 def driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
     yield driver
     driver.quit()
