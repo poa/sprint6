2d42405c7c818a6e4d67928b505500cd94b21296
Oleg Peshko <opeshko@ptsecurity.com>
2024-07-14 14:44:48 +0700

    Add viewport size options for driver

--- conftest.py
+++ conftest.py
@@ -7,6 +7,8 @@ from selenium.webdriver.firefox.options import Options
 def driver():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--width=1280")
+    options.add_argument("--height=1024")
     driver = webdriver.Firefox(options=options)
     yield driver
     driver.quit()
